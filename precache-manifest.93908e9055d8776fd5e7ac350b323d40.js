89b861df69a5e91f56e2af8b889617ee6dde0e1d
Ruth Amaya <ruthamaya91@gmail.com>
2020-03-21 20:41:37 -0500

    Updates

---
+++ precache-manifest.93908e9055d8776fd5e7ac350b323d40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8ca28c8cff5666dae8f8b3fec53240b",
+    "url": "/forum-react/index.html"
+  },
+  {
+    "revision": "c6e5cdc4e5386c9dccd9",
+    "url": "/forum-react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "de220b5e13a13d9ed41d",
+    "url": "/forum-react/static/css/main.744b6dbb.chunk.css"
+  },
+  {
+    "revision": "c6e5cdc4e5386c9dccd9",
+    "url": "/forum-react/static/js/2.0c79d299.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/forum-react/static/js/2.0c79d299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de220b5e13a13d9ed41d",
+    "url": "/forum-react/static/js/main.2a0203e5.chunk.js"
+  },
+  {
+    "revision": "5b5a153fa900135cfa22",
+    "url": "/forum-react/static/js/runtime-main.738bf504.js"
+  }
+]);
